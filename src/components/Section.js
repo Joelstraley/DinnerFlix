43a32935609ecd40ffcdf90454a4700ab3a3d859
Joel Straley <joelstraley@gmail.com>
2022-12-08 03:04:40 -0500

    updating useEffect that is causing issues deploying to netlify

--- src/components/Section.js
+++ src/components/Section.js
@@ -5,18 +5,19 @@ function Section({ genre }) {
   const [movies, setMovies] = useState(null)
   const [pageState, setPageState] = useState(null)
 
+  const fetchData = async () => {
+    const response = await fetch('.netlify/functions/getMovies', {
+      method: 'POST',
+      body: JSON.stringify({ genre: genre, pageState: pageState }),
+    })
+    const responseBody = await response.json()
+    setMovies(responseBody.data.movies_by_genre.values)
+    setPageState(responseBody.data.movies_by_genre.pageState)
+  }
+
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch('.netlify/functions/getMovies', {
-        method: 'POST',
-        body: JSON.stringify({ genre: genre, pageState: pageState }),
-      })
-      const responseBody = await response.json()
-      setMovies(responseBody.data.movies_by_genre.values)
-      setPageState(responseBody.data.movies_by_genre.pageState)
-    }
     fetchData()
-  })
+  }, [])
 
   return (
     <>
