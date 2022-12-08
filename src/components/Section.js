a5c82e57bd91d04f8776c63ae3ada4fbe5ffc2c3
Joel Straley <joelstraley@gmail.com>
2022-12-08 12:44:09 -0500

    adding fix to react-hooks error blocking netlify deployment

--- src/components/Section.js
+++ src/components/Section.js
@@ -5,18 +5,18 @@ function Section({ genre }) {
   const [movies, setMovies] = useState(null)
   const [pageState, setPageState] = useState(null)
 
-  const fetchData = async () => {
-    const response = await fetch('.netlify/functions/getMovies', {
-      method: 'POST',
-      body: JSON.stringify({ genre: genre, pageState: pageState }),
-    })
-    const responseBody = await response.json()
-    setMovies(responseBody.data.movies_by_genre.values)
-    setPageState(responseBody.data.movies_by_genre.pageState)
-  }
-
   useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('.netlify/functions/getMovies', {
+        method: 'POST',
+        body: JSON.stringify({ genre: genre, pageState: pageState }),
+      })
+      const responseBody = await response.json()
+      setMovies(responseBody.data.movies_by_genre.values)
+      setPageState(responseBody.data.movies_by_genre.pageState)
+    }
     fetchData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
